f7011913b2da8af4ee06808533b30ddadfd8b740
LargeSquare <danilyuk.serg88@gmail.com>
2022-12-22 23:40:35 +0300

    Add config for further algorithm

---
+++ Service/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchPrimeNumbers.Service
+{
+    internal static class Config
+    {
+        public static int From { get; set; }
+        public static int To { get; set; }
+    }
+}
