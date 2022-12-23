0da922007241745287832af9f1cac73dfc7c6570
LargeSquare <danilyuk.serg88@gmail.com>
2022-12-23 20:40:22 +0300

    I want try develop other algorithm... Create class for this

---
+++ classes/SieveOfEratosthenes.cs
@@ -0,0 +1,14 @@
+﻿namespace SearchPrimeNumbers.classes
+{
+    internal static class SieveOfEratosthenes
+    {
+        // because RAM is not infinity
+        public static int ClusterSize { get; set; } = 512;
+
+
+        public static void Run()
+        {
+
+        }
+    }
+}
