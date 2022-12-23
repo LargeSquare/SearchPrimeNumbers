be7e03f64dda2abce6d2650e230aa08f9da67962
LargeSquare <danilyuk.serg88@gmail.com>
2022-12-23 03:36:37 +0300

    Okay. Everytime when top limit increase by 2 times than execute time increasing by ~2.8 times. If into "Config.To" set BigInt from 10 row, then (by approximate calculation) algorithm will be executing on my laptop ~281706123480784774629884278636437352374867057593743835136 years.... Not bad... It's better than my python algorithm :D

--- Service/Config.cs
+++ Service/Config.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Numerics;
 
 namespace SearchPrimeNumbers.Service
 {
     internal static class Config
     {
         public static int From { get; set; } = 3;
-        public static int To { get; set; } = 1000000;
+        public static BigInteger To { get; set; } = BigInteger.Parse("800000000");
+        //public static BigInteger To { get; set; } = BigInteger.Parse("39020571855401265608465770302117235537201395138561");
     }
 }
