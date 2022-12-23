e9f4bbc8158f69ba769cf95a5eafe1ba5de4b92b
LargeSquare <danilyuk.serg88@gmail.com>
2022-12-23 21:10:39 +0300

    Update logger. Now you can point the file name. I don't want implement INPC right now, because... 16 and 26 row...

--- Service/Logger.cs
+++ Service/Logger.cs
@@ -2,7 +2,30 @@
 {
     internal class Logger
     {
-        private static string filePath { get; set; } = AppDomain.CurrentDomain.BaseDirectory + "primeNumbers.txt";
+        private static string _baseFileName = "log.txt";
+        private static string _filePath = baseDir + baseFileName;
+
+        private static string baseDir { get; set; } = AppDomain.CurrentDomain.BaseDirectory;
+        public static string baseFileName
+        {
+            get { return _baseFileName; } 
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new NullReferenceException();
+                _baseFileName = value;
+                filePath = baseDir + _baseFileName;
+            }
+        }
+        private static string filePath
+        {
+            get { return _filePath; }
+            set
+            {
+                if (string.IsNullOrEmpty(value)) throw new NullReferenceException();
+                _filePath = value;
+                streamWriter = new StreamWriter(_filePath);
+            }
+        }
         private static StreamWriter streamWriter { get; set; } = new StreamWriter(filePath);
         public static void Log(string message)
         {
