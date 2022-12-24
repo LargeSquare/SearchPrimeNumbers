538bfc0fddbc0a77aeee7532d42db7873b0b2a01
LargeSquare <danilyuk.serg88@gmail.com>
2022-12-24 15:11:17 +0300

    Create database and table models

---
+++ Data/DatabaseModel.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SearchPrimeNumbers.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchPrimeNumbers.Data
+{
+    internal class DatabaseModel : DbContext
+    {
+        public DatabaseModel(DbContextOptions<DatabaseModel> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PrimeNumber> PrimeNumbers => Set<PrimeNumber>();
+    }
+}
---
+++ Data/Tables/PrimeNumber.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace SearchPrimeNumbers.Data.Tables
+{
+    internal class PrimeNumber
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DefaultValue(0)]
+        public int Number { get; set; }
+
+        [Required]
+        [DefaultValue(0)]
+        public int EndOffset { get; set; }
+    }
+}
